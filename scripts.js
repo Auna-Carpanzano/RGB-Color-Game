6e3f5fe34c2b8fe41a7f22c0f343169b64a6a7c0
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-16 16:52:45 -0700

    Create var r, g, and b and set to a random whole number between 0-255

--- scripts.js
+++ scripts.js
@@ -25,5 +25,7 @@ function getRandomColors(num){
 }
 
 function randomColor(){
-
+  var r= Math.floor(Math.random() * 256);
+  var g= Math.floor(Math.random() * 256);
+  var b= Math.floor(Math.random() * 256);
 }
