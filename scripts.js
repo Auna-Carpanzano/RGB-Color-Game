adcc2643d0a357f7b417468b85ffae02d5b639cc
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-16 18:00:49 -0700

    Assign span with id of colorDisplay to have the text of the pickedColor

--- scripts.js
+++ scripts.js
@@ -25,6 +25,7 @@ $("#reset").on("click", function() {
 function reset() {
   colors = getRandomColors(numOfSquares);
   pickedColor= pickColor();
+  $("#colorDisplay").text(pickedColor);
   for(var i=0; i<squares.length; i++){
     if(colors[i]){
       squares[i].style.display= "block";
